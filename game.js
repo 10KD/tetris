5a2ee463d189c15fd8ff593d5fd50e2cfe51a079
KDG <mkim.don@gmail.com>
2017-12-05 10:39:52 -0800

    added requestAnimation

--- game.js
+++ game.js
@@ -6,18 +6,41 @@ context.scale(30, 30);
 context.fillStyle = "#000";
 context.fillRect(0, 0, canvas.width, canvas.height);
 
-const pieceMatrix = [
+const tetrominoS = [
     [0, 0, 0],
     [0, 1, 1],
     [1, 1, 0]
 ];
 
-pieceMatrix.forEach((row, i) => {
-    row.forEach((value, j) => {
-            if (value !== 0) {
-                context.fillStyle = "#33cc33";
-                context.fillRect(j, i, 1, 1);
+const player = {
+    tetromino: tetrominoS,
+    pos: { x: 3, y: 3 },
+};
+
+
+function drawPiece(tetromino, offset) {
+    tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+                if (value !== 0) {
+                    context.fillStyle = "#33cc33";
+                    context.fillRect(
+                        x + offset.x, 
+                        y + offset.y, 
+                        1, 1
+                    );
+                }
             }
-        }
-    )
-});
+        );
+    });
+}
+
+function draw() {
+    drawPiece(player.tetromino, player.pos);
+}
+
+function update() {
+    draw();
+    requestAnimationFrame(update);
+}
+
+update();
